4cbd9bc0be668534c1e3d620d8db9387d4d189e6
Susana_Ribeiro <susanadonribeiro@gmail.com>
2024-02-27 18:15:42 -0300

    Atualizando o projeto, criacao de novos componentes e pages

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,26 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "ANGULAR",
+    "description": "Angular é um popular framework de desenvolvimento front-end mantido pelo Google. Ele é usado para a construção de aplicações web e móveis complexas e dinâmicas.",
+    "photoCover":"https://logowik.com/content/uploads/images/angular9826.logowik.com.webp"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "HTML",
+    "description": "HTML, que significa HyperText Markup Language (Linguagem de Marcação de Hipertexto), é a linguagem padrão utilizada para criar e estruturar páginas web.",
+    "photoCover":"https://media.licdn.com/dms/image/D4D12AQEaTK5_cv5Fmg/article-cover_image-shrink_720_1280/0/1677082453584?e=2147483647&v=beta&t=hy4wcIl2vOpp5lr5k74obYVN63IT-U4lydNOIo3hHGo"
   },
   {
     "id":"3",
-    "title": "Novo filme do pantera negra lançado em breve",
-    "description": "bla blabla",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "CSS",
+    "description": "CSS, que significa Cascading Style Sheets (Folhas de Estilo em Cascata), é uma linguagem utilizada para estilizar a apresentação de documentos HTML (ou XML).",
+    "photoCover":"https://ideacdn.net/idea/ct/82/myassets/blogs/css-nedir-css3.jpg?revision=1581695062"
   },
   {
     "id":"4",
-    "title": "Novo filme dos X-men está em pós produção, afirma diretor",
-    "description": "bla blabla",
-    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+    "title": "TYPESCRIPT",
+    "description": "TypeScript pode ser usado para tornar páginas web mais dinâmicas, oferecendo uma maneira segura e eficiente de manipular o DOM.",
+    "photoCover":"https://dkrn4sk0rn31v.cloudfront.net/uploads/2021/09/TYPESCRIPT.png"
   }
 ]
